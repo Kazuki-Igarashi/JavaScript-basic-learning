f8fbe696cc1f826a9deef71f21653929334dabed
Igarashi <vki.0504@gmail.com>
2024-02-15 13:04:56 +0900

    数値を固定小数点数形式で文字列に変換する(toFixed)

---
+++ tofixed/fixed.js
@@ -0,0 +1,20 @@
+let num = 34.5642;
+
+// console.log(num.toFixed());
+// >> 35
+// console.log(num.toFixed(1));
+// >> 34.6
+// console.log(num.toFixed(3));
+// >> 34.564
+
+let num1 = 1.6e-2;
+
+// console.log(num.toFixed(1));
+// >> 0.0
+// console.log(num.toFixed(3));
+// >> 0.016
+
+let num3 = 728.52;
+
+// console.log(num.toFixed(5));
+// >> 728.52000
