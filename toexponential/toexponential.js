d381092dbae87fc43622fda73c755b643288d95a
Igarashi <vki.0504@gmail.com>
2024-02-15 12:51:20 +0900

    数値を指数形式で文字列に変換する(toExponential)

---
+++ toexponential/toexponential.js
@@ -0,0 +1,22 @@
+let num = 34.5642;
+
+// console.log(num.toExponential());
+// >> 3.45642e+1
+
+// console.log(num.toExponential(2));
+// >> 3.46e+1
+
+// console.log(num.toExponential(4));
+// >> 3.4564e+1
+
+let num2 = 728.52;
+
+
+// console.log(num.toExponential());
+// >> 7.2852e+2
+
+// console.log(num.toExponential(1));
+// >> 7.3e+2
+
+// console.log(num.toExponential(5));
+// >> 7.28520e+2
