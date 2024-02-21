519004a94bf0c31b8e2c8cef9a21c472afbe7571
Igarashi <vki.0504@gmail.com>
2024-02-22 04:55:07 +0900

    文字列リテラルの記述方法

---
+++ string/string.js
@@ -0,0 +1,20 @@
+let name = 'Yamada Tarou';
+let address = "東京都千代田区"
+
+// let name1 = '';
+// let address1 "";
+
+// let msg = 'Tarou's watch';
+// >> Uncaught SyntaxError: Unexpected identifier
+
+let msg = "Tarou's Watch";
+let msg1 = '今日は"特売日"です';
+
+let msg2 = 'Tarou\'s watch';
+let msg3 = "今日は\"特売日\"です";
+
+// 文字列リテラルを入力するとき、途中で Enter キーを押して改行することはできません。
+// let msg4 = 'こんにちは
+// 今日はお元気ですね';
+
+let msg5 = 'こんにちは。\n今日はお元気ですね';
