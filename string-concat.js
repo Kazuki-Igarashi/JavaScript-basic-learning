2d1872e260f64da5f6c36f7b721ebdb6eb3dcc2e
Igarashi <vki.0504@gmail.com>
2024-03-01 10:41:45 +0900

    文字列を連結する(concat)

---
+++ string-concat.js
@@ -0,0 +1,14 @@
+let pref = '東京都';
+let addless;
+addless = pref.concat('港区', '芝公園');
+
+// console.log(address);
+// >> 東京都港区芝公園
+// console.log(pref);
+// >> 東京都
+
+let point = 78;
+let msg = '得点は'.concat(point, '点です');
+
+let point1 = 84;
+let msg1 = '得点は' + point1 + '点です';
