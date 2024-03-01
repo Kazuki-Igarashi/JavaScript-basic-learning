4d4564707a09034d010aa4f291a3c34686ddd227
Igarashi <vki.0504@gmail.com>
2024-03-01 13:11:55 +0900

    文字列から開始位置と終了位置を指定して部分文字列を取得する(slice, substring)

---
+++ string/string/string-substring/substring.js
@@ -0,0 +1,63 @@
+let address = '東京都港区芝公園'
+let sub;
+
+// sub = address.slice(3, 5);
+// console.log(sub);
+// >> 港区
+
+// sub = address.slice(2);
+// console.log(sub);
+// >> 都港区芝公園
+
+// sub = address.slice(-3, -1);
+// console.log(sub);
+// >> 芝公
+
+// sub = address.slice(-5, 5);
+// console.log(sub);
+// >> 港区
+
+// sub = address.slice(3, 4);
+// console.log(sub);
+// >> 港
+
+// sub = address.slice(3, 3);
+// console.log(sub);
+// >> (空文字)
+
+// sub = address.slice(3, 2);
+// console.log(sub);
+// >> (空文字)
+
+// let address = 'JavaScript';
+// let sub;
+
+// sub = address.substring(4, 7);
+// console.log(sub);
+// >> Scr
+
+// sub = address.substring(4);
+// console.log(sub);
+// >> Script
+
+// let address = 'JavaScript';
+// let sub;
+
+// sub = address.substring(-3, 4);
+// console.log(sub);
+// >> Java
+
+// sub = address.substring(4, 15);
+// console.log(sub);
+// >> Script
+
+// let address = 'JavaScript';
+// let sub;
+
+// sub = address.substring(3, 3);
+// console.log(sub);
+// >> (空文字)
+
+// sub = address.substring(6, 3);
+// console.log(sub);
+// >> aSc
