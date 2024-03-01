3f1f608fb5af31a9cb4349a451a13c60085cc5e4
Igarashi <vki.0504@gmail.com>
2024-03-01 13:53:03 +0900

    変数を使ってできること

---
+++ variable/variable.js
@@ -0,0 +1,12 @@
+let tax = 1.1;
+
+// console.log("ボールの値段は" + (800 * tax).toPrecision(3));
+// console.log("バットの値段は" + (1200 * tax).toPrecision(3));
+// console.log("グローブの値段は" + (1700 * tax).toPrecision(3));
+
+// console.log("テストの成績の合計は" + (80 + 72 + 84));
+// console.log("テストの成績の平均は" + ((80 + 72 + 84) / 3).toPrecision(3));
+
+let sum = 80 + 72 + 84;
+// console.log("テストの成績の合計は" + sum);
+// console.log("テストの成績の平均は" + (sum / 3).toPrecision(3));
