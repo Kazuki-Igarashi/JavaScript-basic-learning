20db4f62afa6ff3ee1de181ee2ab00311c252433
Igarashi <vki.0504@gmail.com>
2024-03-12 15:18:24 +0900

    インクリメント演算子/デクリメント演算子

---
+++ operator/operator-3/operator-3.js
@@ -0,0 +1,26 @@
+let num = 5;
+// num = num + 1; // 1 を加える
+// num = num - 1; // 1 を減らす
+
+// num++
+// num--
+
+// for (let i = 0; i < 10; i++){
+//   console.log("i =" + i);
+// }
+
+++num
+
+let num1 = 5;
+let num2;
+
+// num2 = num1++;
+
+// let temp = num1;
+// num1 = num1 + 1;
+// num2 = temp;
+
+// num2 = ++num1;
+
+num1 = num1 + 1;
+num2 = num1;
