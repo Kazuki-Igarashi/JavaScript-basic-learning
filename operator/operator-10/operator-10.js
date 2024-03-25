247e3719f55c1fc7104e12b5e1e4c08d9f197489
Igarashi <vki.0504@gmail.com>
2024-03-26 08:54:42 +0900

    文字列を連結する

---
+++ operator/operator-10/operator-10.js
@@ -0,0 +1,23 @@
+let addless = '東京都' + '千代田区';
+console.log(addless);
+
+let price = 200 + '円';
+console.log(price);
+
+let str = '答えは' + true + 'です。';
+console.log(str);
+
+let price1 = 200;
+console.log('値段は' + price + '円です');
+
+let data = ['Orenge', 'Melon'];
+console.log('商品は' + data + 'です。');
+
+let msg = 100 + 80 + '円です。';
+console.log(msg);
+
+let msg1 = '値段は' + 100 + 80 + '円です';
+console.log(msg1)
+
+let msg2 = '値段は' + (100 + 80) + '円です';
+console.log(msg2)
