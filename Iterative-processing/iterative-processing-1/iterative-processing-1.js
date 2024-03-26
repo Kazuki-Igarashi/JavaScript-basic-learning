4818835e4f2db2d6f761afe6f8208a0f7d3cdfd1
Igarashi <vki.0504@gmail.com>
2024-03-26 10:33:09 +0900

    for文を使った繰り返し処理

---
+++ Iterative-processing/iterative-processing-1/iterative-processing-1.js
@@ -0,0 +1,30 @@
+for (let i = 1; i < 3; i++){
+  console.log("i = " + i);
+}
+
+console.log('end')
+
+for (let i = 3; i < 10; i += 3){
+  console.log("i =" + i)
+}
+
+console.log('end')
+
+for (let i = 10; i > 1; i--){
+  console.log("i = " + i);
+}
+
+console.log('end')
+
+let color = ['Red', 'Blue', 'Green'];
+for (let i = 0; i < color.length; i++){
+  console.log(color[i]);
+}
+
+console.log('end')
+
+for (let i = 1, j = 4; i < 5; i++, j--){
+  console.log("i =" + i + ", j =" + j);
+}
+
+console.log('end')
