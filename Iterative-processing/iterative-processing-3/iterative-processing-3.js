03bc01b1242a93ace9dbeb2864bc2072f463b5e4
Igarashi <vki.0504@gmail.com>
2024-04-01 11:46:10 +0900

    while文を使った繰り返し処理

---
+++ Iterative-processing/iterative-processing-3/iterative-processing-3.js
@@ -0,0 +1,18 @@
+let num = 3;
+while (num < 100){
+  console.log(num);
+  num = num * 3;
+}
+
+console.log('end')
+
+let total = 0;
+let count = 1;
+
+while (total < 10){
+  total += count;
+  count ++;
+  console.log(total);
+}
+
+console.log('end')
