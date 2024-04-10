6d01a14f25f9a5be40838ca977445c4fd4f97ecc
Igarashi <vki.0504@gmail.com>
2024-04-10 16:43:10 +0900

    多次元配列の利用

---
+++ array/array-4/array-4.js
@@ -0,0 +1,11 @@
+let userData = [
+  ['Yamada', 28, 'Tokyo'],
+  ['Suzuki', 35,'Fukushima'],
+  ['Honda', 24, 'Sendai']
+];
+
+let user1 = userData[0];
+console.log(user1);
+console.log(user1[0]);
+console.log(user1[1]);
+console.log(user1[2]);
