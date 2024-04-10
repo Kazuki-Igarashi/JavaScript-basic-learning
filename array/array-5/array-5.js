fa604215e21585d47a95388e6a5b6fc538e8836f
Igarashi <vki.0504@gmail.com>
2024-04-10 16:55:03 +0900

    配列の要素の数を取得する

---
+++ array/array-5/array-5.js
@@ -0,0 +1,17 @@
+let data = [10, 42, 52];
+console.log(data.length);
+
+let data2 = [];
+data2[0] = 10;
+data2[5] = 35;
+
+console.log(data2);
+console.log(data2.length);
+
+data.length = 5;
+console.log(data);
+
+let data3 = [10, 42, 52, 28, 26];
+data3.length = 2;
+
+console.log(data3);
