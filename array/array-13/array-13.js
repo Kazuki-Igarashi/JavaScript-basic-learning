9760d666cb12c3322c357e4883e3f7614e29fc86
Igarashi <vki.0504@gmail.com>
2024-04-19 18:44:32 +0900

    配列の全要素を連結して作成した文字列を取得する

---
+++ array/array-13/array-13.js
@@ -0,0 +1,17 @@
+let fruit = ['Apple', 120, 'Grapes', 80];
+console.log(fruit.join());
+
+console.log(fruit.join(' + '));
+
+console.log(fruit.join(''));
+
+let fruit1 = [['Apple', 120], ['Grapes', 80]];
+
+console.log(fruit1.join(' + '));
+
+let no = [10, 15, , 24];
+console.log(no.join(','));
+
+let fruit2 = ['Apple', 'Grapes', 'Orange'];
+
+console.log(fruit2.toString());
