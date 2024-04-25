ac7717ec95a4ce0f9503851866cb159c4775771b
Igarashi <vki.0504@gmail.com>
2024-04-25 18:44:30 +0900

    配列の要素の中で条件を満たした要素だけを集めて新しい配列を作成する

---
+++ array/array-15/array-15.js
@@ -0,0 +1,9 @@
+let result = [48, 75, 92, 61, 54, 83, 76];
+// let filterResult = result.filter(function(element){
+//   return element > 70;
+// });
+
+let filterResult = result.filter(element => element > 70 );
+
+console.log(filterResult);
+console.log(result)
